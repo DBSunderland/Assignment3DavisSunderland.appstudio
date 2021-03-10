f8e5e16b2d1e1d152bf97060ec8881c8da54d422
DBSunderland <davissunderland@creighton.edu>
2021-03-10 12:36:34 -0600

    12:36 Update

--- forms/cityWhile/cityWhile.js
+++ forms/cityWhile/cityWhile.js
@@ -3,12 +3,17 @@ let doneWithCities = false
 let yesNo = ""
 
 while (doneWithCities == false) {
-  yesNo = prompt("Do you have more names? Yes or No")
+  yesNo = prompt("Do you have more cities? Yes or No")
   if (yesNo == "Yes") {
     doneWithCities = false
-    newName = prompt("Enter the next name")
+    newCity = prompt("Enter the next city")
+    cities.push(newCity)
  } else {
     doneWithCities = true
   }
-  
-  
+}  
+
+/*
+console.log(cities)
+*/
+
